eee8344047cd96cc636f11721ad80a84ce25d98d
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2023-05-30 19:57:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express  from "express";
+import userRouter from "./routes/user.js";
+import taskRouter  from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+
+
+export const app = express();
+
+config({
+    path:"./data/config.env",
+});
+
+
+// Using Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin :[process.env.FRONTEND_URL],
+    methods : ["GET","POST", "PUT", "DELETE"],
+    credentials : true,
+}))
+
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+
+
+
+
+app.get("/", (req,res)=>{
+    res.send("<h1>Nice Working</h1>");
+})  ;
+
+
+// Using error Middleware
+app.use(errorMiddleware);
+
+
+
